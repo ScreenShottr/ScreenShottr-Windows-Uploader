2e678995d0382584992affc1ddcd9d89a6ff02af
TheCJGCJG <charlesgillham@me.com>
2014-09-07 01:36:45 +0100

    Initial Commit

---
+++ ScreenShottr Desktop Uploader/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ScreenShottr_Desktop_Uploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.chooseFile = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.uploadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.resultBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(216, 409);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(448, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // chooseFile
+            // 
+            this.chooseFile.Location = new System.Drawing.Point(670, 409);
+            this.chooseFile.Name = "chooseFile";
+            this.chooseFile.Size = new System.Drawing.Size(75, 23);
+            this.chooseFile.TabIndex = 1;
+            this.chooseFile.Text = "Choose File";
+            this.chooseFile.UseVisualStyleBackColor = true;
+            this.chooseFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 375);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFD";
+            this.openFD.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uploadToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(757, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // uploadToolStripMenuItem
+            // 
+            this.uploadToolStripMenuItem.Name = "uploadToolStripMenuItem";
+            this.uploadToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.uploadToolStripMenuItem.Text = "Upload";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Enabled = false;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Enabled = false;
+            this.resultBox.Location = new System.Drawing.Point(13, 411);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(197, 20);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.Text = "Select an Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 444);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chooseFile);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ScreenShottr Desktop Uploader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button chooseFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem uploadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox resultBox;
+    }
+}
+
---
+++ ScreenShottr Desktop Uploader/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Web;
+
+namespace ScreenShottr_Desktop_Uploader {
+	public partial class Form1: Form {
+		public static string Base64Encode(string plainText) {
+			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+			return System.Convert.ToBase64String(plainTextBytes);
+		}
+
+		public static string Base64Decode(string base64EncodedData) {
+			var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+			return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+		}
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void openFileDialog1_FileOk(object sender, EventArgs e)
+        {
+
+        }
+
+		private void button1_Click(object sender, EventArgs e) {
+			string chosenFile = "";
+            pictureBox1.Image = null;
+            resultBox.Enabled = false;
+            resultBox.Text = "";
+			openFD.Title = "Select an Image to Upload";
+			openFD.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			openFD.Filter = "PNG Images|*.png|JPG Images|*.jpg|JPEG Images|*.jpeg";
+
+			openFD.ShowDialog();
+			chosenFile = openFD.FileName;
+			if (chosenFile != "openFD") {
+
+				System.Drawing.Image imageData = Image.FromFile(chosenFile);
+				pictureBox1.Image = imageData;
+                UploadFile(chosenFile);
+
+			}
+            openFD.FileName = "openFD";
+
+		}
+
+
+        public void UploadFile(string chosenFile)
+        {
+			var client = new WebClient();
+			client.UploadFileCompleted += client_UploadDataCompleted;
+			client.UploadProgressChanged += client_UploadDataChanged;
+            client.UploadFileAsync(new Uri("https://www.screenshottr.us/upload?uploadAr=file"), "POST", @"" + chosenFile + "");
+		}
+
+		void client_UploadDataCompleted(object sender, UploadFileCompletedEventArgs e) {
+			if (e.Error != null) {
+				MessageBox.Show(e.Error.Message);
+			}
+
+			byte[] response = e.Result;
+            progressBar1.Value = 100;
+            resultBox.Enabled = true;
+            resultBox.Text = System.Text.Encoding.Default.GetString(response);
+            Clipboard.SetText(System.Text.Encoding.Default.GetString(response));
+		}
+        void client_UploadDataChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+			int percent = e.ProgressPercentage;
+            progressBar1.Value = percent;
+		}
+	}
+
+}
